160844b9dc8a89c517564a64c0125c41cf04c574
Anansdraj |agadeesan <anandrajj@gmail.com>
2014-05-20 23:05:56 +0530

    Delete README.R

--- README.R
+++
@@ -1,5 +0,0 @@
-Getting & Cleaning Data - Project
-========================================================
-
-This document helps to understand the R script run_analysis.R developed for the course project. 
-
